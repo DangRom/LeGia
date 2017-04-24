1317b03c9690996fa68201ded0d561fa27066460
DangRom <phamtandang@gmail.com>
2017-04-24 16:09:53 +0700

    finish category-index view

--- LeGia/Areas/Admin/Controllers/CategoryController.cs
+++ LeGia/Areas/Admin/Controllers/CategoryController.cs
@@ -13,5 +13,10 @@ namespace LeGia.Areas.Admin.Controllers
         {
             return View();
         }
+
+        public IActionResult New()
+        {
+            return View();
+        }
     }
 }
