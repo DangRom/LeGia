9b488bf2093a75b01618ddcda826a95e71bd7a10
DangRom <phamtandang@gmail.com>
2017-05-03 14:19:55 +0700

    finish image

---
+++ LeGia/Areas/Admin/Controllers/ImageController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeGia.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ImageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
