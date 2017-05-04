583df563d0a0a7dbfe125318c23bd191d5f9bfb6
DangRom <phamtandang@gmail.com>
2017-05-04 15:58:36 +0700

    finished infactstructure

--- LeGia/Services/InfactStructure/IRepositoriesBase.cs
+++ LeGia/Services/InfactStructure/IRepositoriesBase.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dapper;
+using System.Data;
 
 namespace LeGia.Services.InfactStructure
 {
-    public interface IRepositoriesBase
+    public interface IRepositoriesBase<T> where T : class
     {
+        void Execute(string sp, DynamicParameters para);
+        bool CheckRecord(string sp, DynamicParameters para);
+        T GetById(string sp, DynamicParameters para);
+        IEnumerable<T> GetAll(string sp, DynamicParameters para);
     }
 }
--- LeGia/Services/InfactStructure/RepositoriesBase.cs
+++ LeGia/Services/InfactStructure/RepositoriesBase.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using Dapper;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace LeGia.Services.InfactStructure
 {
-    public class RepositoriesBase
+    public abstract class RepositoriesBase<T> : IRepositoriesBase<T> where T : class
     {
+        private IDbConnection _db;
+        protected IDbConnection Db
+        {
+            get
+            {
+                string cnn = $"ConnectionString = Configuration[Database:ConnectionString]";
+                return _db ?? (_db = new MySql.Data.MySqlClient.MySqlConnection(cnn));
+            }
+        }
+
+        public bool CheckRecord(string sp, DynamicParameters para)
+        {
+            var check = Db.Query<int>(sp, para, null, false, null, CommandType.StoredProcedure);
+            return bool.Parse(check.ToString());
+        }
+
+        public void Execute(string sp, DynamicParameters para)
+        {
+            Db.Execute(sp, para, null, null, CommandType.StoredProcedure);
+        }
+
+        public IEnumerable<T> GetAll(string sp, DynamicParameters para)
+        {
+            return Db.Query<T>(sp, para, null, false, null, CommandType.StoredProcedure).ToList();
+        }
+
+        public T GetById(string sp, DynamicParameters para)
+        {
+            return Db.Query<T>(sp, para, null, false, null, CommandType.StoredProcedure).FirstOrDefault();
+        }
     }
 }
